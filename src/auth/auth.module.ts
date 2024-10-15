863e76465e3f5e57fe2128483cc9bab99a7f2153
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-10-15 21:26:09 +0700

    Fix auth module cannot load .env variable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,16 @@ import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: process.env.JWT_EXPIRATION || '3600s' },
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        global: true,
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EXPIRATION || '10m' },
+      }),
     }),
   ],
   controllers: [AuthController],
